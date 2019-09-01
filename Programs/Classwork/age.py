1bc01296e245da9c6a1a6cda6b212436a6fb38ee
Oscar Lopez <oscarl.developer@gmail.com>
2019-09-01 09:20:32 -0700

    Added some directories for better management of files.

---
+++ Programs/Classwork/age.py
@@ -0,0 +1,4 @@
+import sys
+
+age = input("How old are you?\n")
+print("%s you are %s years old."%(sys.argv[1], age))
---
+++ Programs/Classwork/hello_2.py
@@ -0,0 +1,5 @@
+import sys
+
+print(sys.argv)
+print(len(sys.argv))
+print('Hello %s'%(sys.argv[1]))
