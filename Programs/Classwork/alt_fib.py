2b9a365ce13e09fe1e328a303ebc2a656c0b051c
Oscar Lopez <oscarl.developer@gmail.com>
2019-10-05 12:10:32 -0700

    Added alt_fib.py and fibonacci.py

---
+++ Programs/Classwork/alt_fib.py
@@ -0,0 +1,12 @@
+fib_dict = dict()
+
+def alt_fib(n, a_dict):
+    if n <= 2 and n >= 1:
+        return 1
+    else:
+        if n in a_dict:
+            return a_dict[n]
+        else:
+            return alt_fib(n-1, a_dict) + alt_fib(n-2, a_dict)
+
+print(alt_fib(5, fib_dict))
---
+++ Programs/Classwork/fibonacci.py
@@ -0,0 +1,13 @@
+"""
+Simple fibonacci simulation code.
+
+Author: Oscar Lopez.
+"""
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(6))
