b2f79bc99bad08c69178260cedb2605e2796ca20
Oscar Lopez <oscarl.developer@gmail.com>
2019-10-12 09:08:41 -0700

    Cleaned up alt_fib.py

--- Programs/Classwork/alt_fib.py
+++ Programs/Classwork/alt_fib.py
@@ -6,7 +6,7 @@ def alt_fib(n, a_dict):
     else:
         if n in a_dict:
             return a_dict[n]
-        else:
-            return alt_fib(n-1, a_dict) + alt_fib(n-2, a_dict)
+        return alt_fib(n-1, a_dict) + alt_fib(n-2, a_dict)
+
 
 print(alt_fib(5, fib_dict))
